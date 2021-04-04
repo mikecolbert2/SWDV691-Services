c210cb72913a2ef9e9f05c3367f3684f5d54b9f5
Mike Colbert <mcolbert1@live.maryville.edu>
2021-04-03 20:58:08 -0500

    closed issue #1
    this references issue #1

--- source_code/queries.js
+++ source_code/queries.js
@@ -10,9 +10,28 @@ const pool = new Pool({
     ssl:{ rejectUnauthorized: false }
 });
 
+// ** Accounts & Authentication ** //
+
 
 const getAllItems = (request, response) => {
-    pool.query('SELECT * FROM items ORDER BY item_name ASC', (error, results) => {
+    pool.query(`SELECT * FROM items ORDER BY item_name ASC`, (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+
+
+  // ** Admin ** //
+
+  const getAllUsers = (request, response) => {
+    pool.query(`SELECT users.first_name, users.last_name, users.email, users.password, 
+                roles.role_name, users.date_created, users.last_login 
+                FROM users 
+                INNER JOIN roles ON users.role_id = roles.role_id 
+                ORDER BY users.last_name ASC, users.first_name ASC;`, (error, results) => {
       if (error) {
         throw error
       }
@@ -21,7 +40,8 @@ const getAllItems = (request, response) => {
   }
 
   module.exports = {
-    getAllItems
+    getAllItems,
+    getAllUsers
   }
 
 
--- source_code/server.js
+++ source_code/server.js
@@ -1,14 +1,11 @@
 // Set up
-var express = require('express');
-var app = express();
-//var mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var cors = require('cors');
-const db = require('./queries')
+const methodOverride = require('method-override');
+const cors = require('cors');
 
-// Configuration
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/MyHabitStacker");
+const db = require('./queries')
 
 app.use(bodyParser.urlencoded({'extended': 'true'}));
 app.use(bodyParser.json());
@@ -24,270 +21,23 @@ app.use(function (req, res, next) {
     next();
 });
 
-// Connect to Postgres
-// pool
-//   .connect()
-//   .then(() => console.log("Postgres connected ..."))
-//   .catch((err) => console.log(err));
-
-//   pool.query("SELECT * FROM items", (err, res) => {
-//     if (err) {
-//       console.log("pg returned an error");
-//       throw error;
-//     }
-//     if (res) {
-//       console.log("pg returned a result from the SQL query");
-//     }
-//   });
-
-
-// Models
-// var Item = mongoose.model('Item', {
-//     item_id: Number,
-//     item_name: String,
-//     item_age: Number
-// });
-
-// var Log = mongoose.model('DailyLog', {
-//     habit_name: String,
-//     last_completed_date: Date
-// });
-
+// root route
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API server' })
   })
 
 
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/index.html'));
-//   });
-
-
-// ** HABITS ** //
+// ** TEST **
 // Get all 
 app.get('/api/items', db.getAllItems)
 
-// app.get('/api/items', function (req, res) {
-
-    // items = [];
-    
-    // items = [
-    //     {
-    //         name: 'test',
-    //         age: 50
-    //     }, 
-    //     {
-    //         name: 'test2',
-    //         age: 48
-    //     } 
-    // ];
-    
-
-    // pool.query('SELECT * FROM items', (err, result) => {
-    //     if (err) {
-    //       return console.error('Error executing query', err.stack)
-    //     }
-    //     items=result.rows;
-    //     console.log(result.rows)
-    //   })
-
-    // console.log("get request ...");
-    // res.json(items)
-
-    // pg.connect(connectionString, function(err, client, done) {
-    //     client.query('SELECT * FROM your_table', function(err, result) {
-    //        done();
-    //        if(err) return console.error(err);
-    //        console.log(result.rows);
-    //     });
-    //  });
 
 
-    // //use mongoose to get data from the database
-    // Habit.find(function (err, habits) {
-    //     // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-    //     if (err) {
-    //         res.send(err);
-    //     }
 
-    //     res.json(habits); // return all habits in JSON format
-    // });
-// });
+// ** ADMIN ** //
 
-// // Get one habit
-// app.get('/api/habits/:id', function (req, res) {
-
-//     console.log("List one specific habit ...");
-    
-//     //use mongoose to get a single habit from the database
-//     Habit.find({ _id: req.params.id }, function (err, habits) {
-
-//         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-//         if (err) {
-//             res.send(err);
-//         }
-
-//         res.json(habits); // return the habit in JSON format
-//     });
-// });
-
-// // Create a new habit
-// app.post('/api/habits', function (req, res) {
-
-//     console.log("Creating a new habit ...");
-
-//     Habit.create({
-//         habit_name: req.body.name,
-//         start_date: req.body.date,
-//         done: false
-//     }, function (err, habit) {
-//         if (err) {
-//             res.send(err);
-//         }
-
-//         // create and return all the habits
-//         Habit.find(function (err, habits) {
-//             if (err)
-//                 res.send(err);
-//             res.json(habits);
-//         });
-//     });
-
-// });
-
-// // Update a habit 
-// app.put('/api/habits/:id', function (req, res) {
-//     const habit = {
-//         habit_name: req.body.name
-//     };
-//     console.log("Updating the habit ... ", req.params.id);
-//     Habit.update({_id: req.params.id}, habit, function (err, raw) {
-//         if (err) {
-//             res.send(err);
-//         }
-//         res.send(raw);
-//     });
-// });
-
-// // Delete a habit
-// app.delete('/api/habits/:id', function (req, res) {
-//     console.log("Deleting a habit ... ", req.params.id);
-//     Habit.findOneAndRemove({   //updated from .remove - https://stackoverflow.com/questions/50283081/mongodb-error-cannot-use-retryable-writes-with-limit-0
-//         _id: req.params.id
-//     }, function (err, habit) {
-//         if (err) {
-//             console.error("Error deleting the habit ", err);
-//         }
-//         else {
-//             Habit.find(function (err, habits) {
-//                 if (err) {
-//                     res.send(err);
-//                 }
-//                 else {
-//                     res.json(habits);
-//                 }
-//             });
-//         }
-//     });
-// });
-
-
-// // ** DAILY LOG ** //
-// // Get all daily log entires 
-// app.get('/api/logs', function (req, res) {
-
-//     console.log("Listing daily log entries ...");
-
-//     //use mongoose to get data from the database
-//     Log.find(function (err, logs) {
-//         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-//         if (err) {
-//             res.send(err);
-//         }
-
-//         res.json(logs); // return all daily entries in JSON format
-//     });
-// });
-
-// // Get last daily entry record
-// app.get('/api/logs/latest-entry', function (req, res) {
-
-//     console.log("List latest daily log entry ...");
-    
-//     //use mongoose to get a single log entry from the database
-//     Log.findOne( {}, {}, {sort: { 'last_completed_date' : -1 } }, function (err, logs) {
-
-//         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-//         if (err) {
-//             res.send(err);
-//         }
-
-//         res.json(logs); // return the habit in JSON format
-//     });
-// });
-
-// // Create a new daily log entry
-// app.post('/api/logs', function (req, res) {
-
-//     console.log("Creating a new daily log entry ...");
-//     console.log(req.body.habit_name)
-//     console.log(req.body.last_completed_date)
-
-//     Log.create({
-//         habit_name: req.body.habit_name,
-//         last_completed_date: req.body.last_completed_date,
-//         done: false
-//     }, function (err, log) {
-//         if (err) {
-//             res.send(err);
-//         }
-
-//         // create and return all the log entries
-//         Log.find(function (err, logs) {
-//             if (err)
-//                 res.send(err);
-//             res.json(logs);
-//         });
-//     });
-
-// });
-
-// // Update a daily log entry 
-// app.put('/api/logs/:id', function (req, res) {
-//     const log = {
-//         habit_name: req.body.name,
-//         last_completed_date: req.body.date
-//     };
-//     console.log("Updating the daily log entry ... ", req.params.id);
-//     Log.update({_id: req.params.id}, log, function (err, raw) {
-//         if (err) {
-//             res.send(err);
-//         }
-//         res.send(raw);
-//     });
-// });
-
-// // Delete a daily log entry
-// app.delete('/api/logs/:id', function (req, res) {
-//     console.log("Deleting a daily log entry ... ", req.params.id);
-//     Log.findOneAndRemove({   //updated from .remove - https://stackoverflow.com/questions/50283081/mongodb-error-cannot-use-retryable-writes-with-limit-0
-//         _id: req.params.id
-//     }, function (err, log) {
-//         if (err) {
-//             console.error("Error deleting the log entry ", err);
-//         }
-//         else {
-//             Log.find(function (err, logs) {
-//                 if (err) {
-//                     res.send(err);
-//                 }
-//                 else {
-//                     res.json(logs);
-//                 }
-//             });
-//         }
-//     });
-// });
+// get all users
+app.get('/api/admin/users', db.getAllUsers)
 
 
 // Start app and listen on port 8080  
