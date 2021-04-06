9c62026e8842a4189868056f964910c733e15411
Mike Colbert <mcolbert1@live.maryville.edu>
2021-04-06 12:40:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,43 @@
 // Set up
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const cors = require("cors");
 
-const db = require('./queries')
+const db = require("./db");
 
-app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.urlencoded({ extended: "true" }));
 app.use(bodyParser.json());
-app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(methodOverride());
 app.use(cors());
 
 //CORS configuration
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, POST, PUT');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, POST, PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 // root route
-app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API server' })
-  })
-
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API server" });
+});
 
 // ** TEST **
-// Get all 
-app.get('/api/items', db.getAllItems)
-
-
-
+// Get all
+app.get("/api/items", db.getAllItems);
 
 // ** ADMIN ** //
 
 // get all users
-app.get('/api/admin/users', db.getAllUsers)
+app.get("/api/admin/users", db.getAllUsers);
 
-
-// Start app and listen on port 8080  
+// Start app and listen on port 8080
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
